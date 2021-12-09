93dde98295da715ee1fe8bef254cf91cfc17edff
Elvan H <elvanhd@gmail.com>
2021-12-10 01:21:52 +0700

    created an example of proxy instance

--- app.js
+++ app.js
@@ -2,17 +2,40 @@ const app = Vue.createApp({
   data() {
     return {
       currentUserInput: '',
-      message: 'Vue is great!'
+      message: 'Vue is great!',
     };
   },
+
   methods: {
     saveInput(event) {
       this.currentUserInput = event.target.value;
     },
+
     setText() {
       this.message = this.currentUserInput;
-    }
-  }
+    },
+  },
 });
 
 app.mount('#app');
+
+const data = {
+  message: 'Hello!',
+  longMessage: 'Hello, World!',
+};
+
+const handler = {
+  set(target, key, value) {
+    if (key === 'message') {
+      target.longMessage = value + ' World!';
+    }
+
+    target.message = value;
+  },
+};
+
+const proxy = new Proxy(data, handler);
+
+proxy.message = 'Hello Vue';
+
+console.log(proxy.longMessage);
