3c4a6adb4fe0fd5b31d6e41fb5b01e8644b4b421
Elvan H <elvanhd@gmail.com>
2021-12-10 05:55:36 +0700

    lifecycle hook examples

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = Vue.createApp({
       message: 'Vue is great!',
     };
   },
+
   methods: {
     saveInput(event) {
       this.currentUserInput = event.target.value;
@@ -14,9 +15,45 @@ const app = Vue.createApp({
       this.message = this.$refs.userText.value;
     },
   },
+
+  beforeCreate() {
+    console.log('beforeCreate');
+  },
+
+  created() {
+    console.log('created');
+  },
+
+  beforeMount() {
+    console.log('beforeMount');
+  },
+
+  mounted() {
+    console.log('mounted');
+  },
+
+  beforeUpdate() {
+    console.log('beforeUpdate');
+  },
+
+  updated() {
+    console.log('updated');
+  },
+
+  beforeUnmount() {
+    console.log('beforeUnmount');
+  },
+
+  unmounted() {
+    console.log('unmounted');
+  },
 });
 
 app.mount('#app');
+
+setTimeout(() => {
+  app.unmount();
+}, 3000);
 
 const app2 = Vue.createApp({
   template: `<p>{{ favoriteMeal }}</p>`,
